9f78cd381a757881736b987b7821c2f8d825474a
Isaac <diarraisaac312@icloud.com>
2024-10-20 19:25:55 +0200

    firt commit

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 const express = require('express');
-const { getCurrentDate } = require('./date');
-const { getAppMessage } = require('./app');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const currentPath = process.cwd();
 
-app.get('/', (req, res) => {
-    const currentDateMessage = getCurrentDate();
-    const appMessage = getAppMessage();
-    res.send(`${appMessage}<br>${currentDateMessage}`); 
+function getCurrentDate() {
+    const now = new Date();
+    return {
+        date: now.toISOString().split('T')[0], 
+        dayOfYear: Math.floor((now - new Date(now.getFullYear(), 0, 0)) / 86400000),
+    };
+}
+
+app.get('/files', async function (req, res) {
+    try {
+        const directory = await fs.promises.readdir(currentPath);
+        const files = [];
+
+        for (const element of directory) {
+            const stats = await fs.promises.stat(path.join(currentPath, element));
+            if (stats.isFile()) {
+                files.push({
+                    name: element,
+                    size: stats.size,
+                    lastUpdate: stats.mtime.toISOString(),
+                });
+            }
+        }
+
+        const currentDate = getCurrentDate();
+        res.send({
+            files,
+            currentDate
+        });
+    } catch (err) {
+        res.status(500).send('Erreur lors de la lecture du répertoire');
+    }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
 });
